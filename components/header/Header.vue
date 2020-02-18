73b04d79c80d2628ad236a34a3c834949debc1ad
Huynhhuynh <conghieuhuynh91@gmail.com>
2020-02-18 08:56:06 +0700

    First commit

---
+++ components/header/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <header id="site-header" class="main-header">
+        <div class="header-top">
+            <div class="site-container">
+                <div class="header-top--menu">
+                    <ul class="menu">
+                        <li class="upcoming-item">
+                            <span class="__tag">Upcoming Tutorial</span>
+                            <a href="#">Video tutorial how to setup new WordPress eCommerce website</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Docs</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Get Support</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Sales</a>
+                        </li>
+                        <li class="item">
+                            <a href="#">Sign in</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="site-container">
+            <div class="header-content">
+                <Brand />
+                <Navigation />
+                <div class="site-tools">
+                    <ul>
+                        <li>
+                            <nuxt-link to="register" class="button">{{ $t('register') }}</nuxt-link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+import Brand from '~/components/header/Brand.vue'
+import Navigation from '~/components/header/Navigation.vue'
+
+export default {
+    components: {
+       Brand,
+       Navigation
+    }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+	<div id="page">
+		<Header />
+		<main id="site-main">
+			<nuxt />
+		</main>
+		<Footer />
+	</div>
+</template>
+
+<script>
+import Header from '~/components/header/Header.vue'
+import Footer from '~/components/footer/Footer.vue'
+
+export default {
+	components: {
+		Header,
+		Footer
+	}
+}
+</script>
