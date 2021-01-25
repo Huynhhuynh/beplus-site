b69666d6e329e2ed6b4a3550928c49e831510856
Hieuhuynh <conghieuhuynh91@gmail.com>
2021-01-25 09:40:19 +0700

    Update project detail

--- pages/portfolio/_slug.vue
+++ pages/portfolio/_slug.vue
@@ -1,11 +1,74 @@
 <template>
   <div>
-    
+    <pre v-if="_project">
+      {{ _project }}
+    </pre>
   </div>
 </template>
 
 <script>
+import { gql } from 'graphql-request'
+
 export default {
-  
+  components: {},
+  async asyncData ( { $graphql, params } ) {
+    console.log( params )
+    const query = gql`
+    query MyProject {
+      project(id: "${ params.slug }", idType: SLUG) {
+        id
+        databaseId
+        content
+        title
+        slug
+        featuredImage {
+          node {
+            mediaItemUrl
+          }
+        }
+        projectTags {
+          nodes {
+            name
+          }
+        }
+        portfolioSettings {
+          buildWidth
+          dateStart
+          websiteUrl
+          participants {
+            position
+            member {
+              ... on Team {
+                id
+                title
+                featuredImage {
+                  node {
+                    mediaItemUrl
+                  }
+                }
+              }
+            }
+          }
+        }
+        portfolioSidebarMetabox {
+          fullPreviewImage {
+            altText
+            mediaItemUrl
+          }
+        }
+        projectId
+      }
+    }
+    `
+    const project = await $graphql.request( query )
+    console.log( project )
+    return { project: project.project }
+  },
+  computed: {
+    _project () {
+      if( ! this.project ) return
+      return this.project
+    }
+  }
 }
 </script>
